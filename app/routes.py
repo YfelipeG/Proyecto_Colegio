d8ca375d7cede7e2602b0c3e20027aadc88caa80
YfelipeG <yfelipegomez@soyucn.edu.co>
2024-08-27 19:54:35 -0500

    presentacion de resultados de contenido en una plantilla

--- app/routes.py
+++ app/routes.py
@@ -1,25 +1,9 @@
+from flask import render_template
+
 from app import app
 
 @app.route("/")
 def index():
-    return "Hola a todos!"
+    return render_template("index.html", titulo="inicio",nombre="felipe")
 
-@app.route("/otro-saludo")
-def otro_saludo():
-    return "Hello!"
-
-@app.route("/saludo/<nombre>")
-def saludo(nombre):
-    
-    contenido = """
-    <html>
-        <head>
-            <title>Saludo</title>
-        </head>
-        <body>
-            <h1>Hola, {}!</h1>
-        </body>
-    </html>
-    """
-
-    return contenido.format(nombre)
+ 
