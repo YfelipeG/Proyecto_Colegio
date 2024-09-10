330a4ab5c47419d6deb87b1247db642c64d8ef16
YfelipeG <yfelipegomez@soyucn.edu.co>
2024-09-09 21:36:56 -0500

    eliminacion de funciones devista no necesarias

--- app/routes.py
+++ app/routes.py
@@ -5,14 +5,3 @@ from app import app
 @app.route("/")
 def index():
     return render_template("index.html", titulo="inicio",nombre="Felipe")
-
-@app.route("/hola/<nombre>")
-def hola(nombre):
-    return render_template("hola.html", Titulo= 'Hola', nombre=nombre)
-
-@app.route("/lenguajes-programacion")
-def lenguajes():
-    lenguajes = ['Python', 'Java', 'JavaScript', 'C#', 'PHP']
-    return render_template("lenguajes.html", titulo="Lenguajes de programación", lenguajes=lenguajes)
-
- 
