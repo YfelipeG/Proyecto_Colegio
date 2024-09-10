db4f0cffb388de2704cfa9249ee783ed81b30ecd
YfelipeG <yfelipegomez@soyucn.edu.co>
2024-09-09 19:19:47 -0500

    uso del condicional con exoresion de jinja

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,6 @@ from app import app
 
 @app.route("/")
 def index():
-    return render_template("index.html", titulo="inicio",nombre="juan")
+    return render_template("index.html", titulo="inicio",nombre="Felipe")
 
  
