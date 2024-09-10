1b6a537bd8a720a2265de2ac632881bb02db805d
YfelipeG <yfelipegomez@soyucn.edu.co>
2024-09-09 21:09:54 -0500

    mejor organizacion de una aplicacion Flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,3 @@
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -6,13 +5,5 @@ app = Flask(__name__)
 
 db = SQLAlchemy(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-
-    def __repr__(self):
-        return f'<User {self.username}>'
-    
 
 from app import routes
