043151e3d9bfaec1dd8d99628ba86252d74ad98d
YfelipeG <yfelipegomez@soyucn.edu.co>
2024-09-11 22:20:20 -0500

    ruta para la vista de gestion de colegio

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,7 @@ from app import app
 @app.route("/")
 def index():
     return render_template("index.html", titulo="inicio",nombre="Felipe")
+
+@app.route("/colegio", getattr=["GET"])
+def colegio():
+    return render_template("colegio.html", titulo="colegio")
