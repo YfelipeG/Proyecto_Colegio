fd2fd3b5cc592e51a1f43d20300477adf9eb7bca
YfelipeG <yfelipegomez@soyucn.edu.co>
2024-09-16 16:28:27 -0500

    Modelo de base de datos funcionando

--- app/models.py
+++ app/models.py
@@ -3,106 +3,131 @@ from sqlalchemy import event
 
 from app import app, db
 from sqlalchemy import func
-
-
-class Estudiante(db.Model):
-    """
-    Representa un estudiante.
-    """
-
-    __tablename__ = 'estudiante'
-    documento = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(45), nullable=False)
-    apellidos = db.Column(db.String(45), nullable=False)
-    telefono = db.Column(db.String(45))
-    email = db.Column(db.String(45), unique=True)
-    direccion = db.Column(db.String(255))
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
-    colegio_id = db.Column(db.Integer, db.ForeignKey('colegio.id'))
-    grado_id = db.Column(db.Integer, db.ForeignKey('grado.id'))
-
-    def __repr__(self):
-        return f'<Estudiante {self.nombre} {self.apellidos}>'
-    
-
+  
 
 class Colegio(db.Model):
     __tablename__ = 'colegio'
+
     id = db.Column(db.Integer, primary_key=True)
     nit = db.Column(db.String(32), unique=True, nullable=False)
-    nombre = db.Column(db.String(45), nullable=False)
+    nombre = db.Column(db.String(64), nullable=False)
     direccion = db.Column(db.String(255))
     telefono = db.Column(db.String(16))
     correo = db.Column(db.String(64), unique=True)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
 
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    # Relaciones
+    estudiantes = db.relationship('Estudiante', back_populates='colegio')
+    grados = db.relationship('Grado', back_populates='colegio')
+    usuarios = db.relationship('Usuario', back_populates='colegio')
 
     def __repr__(self):
         return f'<Colegio {self.nombre}>'
 
 
+class Estudiante(db.Model):
+    __tablename__ = 'estudiante'
 
-class Materia(db.Model):
-    __tablename__ = 'materia'
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(45), nullable=False)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
-    estudiante_documento = db.Column(db.Integer, db.ForeignKey('estudiante.documento'))
+    documento = db.Column(db.Integer, primary_key=True)
+    nombres = db.Column(db.String(45), nullable=False)
+    apellidos = db.Column(db.String(45), nullable=False)
+    telefono = db.Column(db.String(45))
+    email = db.Column(db.String(45), unique=True)
+    direccion = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+    colegio_id = db.Column(db.Integer, db.ForeignKey('colegio.id'))
+    grado_id = db.Column(db.Integer, db.ForeignKey('grado.id'))
+
+    # Relaciones
+    colegio = db.relationship('Colegio', back_populates='estudiantes')
+    grado = db.relationship('Grado', back_populates='estudiantes')
+    usuarios = db.relationship('Usuario', back_populates='estudiante')
 
     def __repr__(self):
-        return f'<Materia {self.nombre}>'
-    
-
+        return f'<Estudiante {self.nombres} {self.apellidos}>'
 
 
 class Grado(db.Model):
     __tablename__ = 'grado'
+
     id = db.Column(db.Integer, primary_key=True)
     grado = db.Column(db.String(45), nullable=False)
     descripcion = db.Column(db.String(255))
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
     colegio_id = db.Column(db.Integer, db.ForeignKey('colegio.id'))
     materia_id = db.Column(db.Integer, db.ForeignKey('materia.id'))
 
+    # Relaciones
+    estudiantes = db.relationship('Estudiante', back_populates='grado')
+    colegio = db.relationship('Colegio', back_populates='grados')
+    materia = db.relationship('Materia', back_populates='grados')
+
     def __repr__(self):
         return f'<Grado {self.grado}>'
-    
 
 
+class Materia(db.Model):
+    __tablename__ = 'materia'
 
-class Usuario(db.Model):
-    __tablename__ = 'usuario'
     id = db.Column(db.Integer, primary_key=True)
-    documento = db.Column(db.String(16), unique=True, nullable=False)
-    password = db.Column(db.String(45), nullable=False)
     nombre = db.Column(db.String(45), nullable=False)
-    apellidos = db.Column(db.String(45), nullable=False)
-    email = db.Column(db.String(64), unique=True, nullable=False)
-    direccion = db.Column(db.String(255))
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
-    rol_id = db.Column(db.Integer, db.ForeignKey('rol.id'))
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+
+    # Relaciones
+    grados = db.relationship('Grado', back_populates='materia')
 
     def __repr__(self):
-        return f'<Usuario {self.nombre} {self.apellidos}>'
-    
+        return f'<Materia {self.nombre}>'
 
 
 class Rol(db.Model):
     __tablename__ = 'rol'
+
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(45), nullable=False)
     descripcion = db.Column(db.String(255))
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+
+    # Relaciones
+    usuarios = db.relationship('Usuario', back_populates='rol')
 
     def __repr__(self):
         return f'<Rol {self.nombre}>'
-    
+
+
+class Usuario(db.Model):
+    __tablename__ = 'usuario'
+
+    id = db.Column(db.Integer, primary_key=True)
+    documento = db.Column(db.String(16), unique=True, nullable=False)
+    password = db.Column(db.String(45), nullable=False)
+    nombre = db.Column(db.String(45), nullable=False)
+    apellidos = db.Column(db.String(32), nullable=False)
+    telefono = db.Column(db.String(16))
+    email = db.Column(db.String(64), unique=True)
+    direccion = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+    rol_id = db.Column(db.Integer, db.ForeignKey('rol.id'))
+    colegio_id = db.Column(db.Integer, db.ForeignKey('colegio.id'))
+    estudiante_documento = db.Column(db.Integer, db.ForeignKey('estudiante.documento'))
+
+    # Relaciones
+    rol = db.relationship('Rol', back_populates='usuarios')
+    colegio = db.relationship('Colegio', back_populates='usuarios')
+    estudiante = db.relationship('Estudiante', back_populates='usuarios')
+
+    def __repr__(self):
+        return f'<Usuario {self.nombre} {self.apellidos}>'
+
+
+
+
 
 def insert_initial_values():
     if not Colegio.query.first():
