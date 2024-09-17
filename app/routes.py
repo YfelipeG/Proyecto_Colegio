92701ef80ec02bab58c3cfa8c316161a1c0b90f0
YfelipeG <yfelipegomez@soyucn.edu.co>
2024-09-16 22:35:39 -0500

    Eliminacion de un Colegio a partir de su ID

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template
 
-from app import app
+from app import app, db
 
 from app.models import Colegio
 
@@ -12,6 +12,15 @@ def index():
 @app.route("/colegio", methods=["GET"])
 def tipos_colegio():
     tipos_colegio = Colegio.query.all()
-    print(tipos_colegio)
-    print(len(tipos_colegio))
     return render_template("colegio.html", titulo="Colegios", tipos_colegio=tipos_colegio)
+
+@app.route("/colegio/<int:id>", methods=["DELETE"])
+def eliminar_colegio(id):
+    colegio = Colegio.query.get(id)
+
+    if colegio is None:
+        return "Colegio no encontrado", 404
+    
+    db.session.delete(colegio)
+    db.session.commit()
+    return "Colegio eliminado", 200
